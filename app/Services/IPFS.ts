751be9f7bbf3fbd783a46241afba2e5d2f094d8c
jalil <jalil@1001.digital>
2023-11-23 22:08:00 +0100

    Implement libp2p

--- app/Services/IPFS.ts
+++ app/Services/IPFS.ts
@@ -17,20 +17,64 @@ class IFPS {
       { json },
       { FsBlockstore },
       { FsDatastore },
+      { noise },
+      { yamux },
+      { bootstrap },
+      { tcp },
+      { createLibp2p },
+      { identifyService },
     ] = await Promise.all([
       // FIXME: Proper ESM imports
       await eval(`import('helia')`),
       await eval(`import('@helia/json')`),
       await eval(`import('blockstore-fs')`),
       await eval(`import('datastore-fs')`),
+      await eval(`import('@chainsafe/libp2p-noise')`),
+      await eval(`import('@chainsafe/libp2p-yamux')`),
+      await eval(`import('@libp2p/bootstrap')`),
+      await eval(`import('@libp2p/tcp')`),
+      await eval(`import('libp2p')`),
+      await eval(`import('libp2p/identify')`),
     ])
 
     const blockstore = new FsBlockstore(Application.tmpPath('block'))
     const datastore = new FsDatastore(Application.tmpPath('data'))
 
+    const libp2p = await createLibp2p({
+      datastore,
+      addresses: {
+        listen: [
+          '/ip4/127.0.0.1/tcp/0'
+        ]
+      },
+      transports: [
+        tcp()
+      ],
+      connectionEncryption: [
+        noise()
+      ],
+      streamMuxers: [
+        yamux()
+      ],
+      peerDiscovery: [
+        bootstrap({
+          list: [
+            '/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
+            '/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa',
+            '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
+            '/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt'
+          ]
+        })
+      ],
+      services: {
+        identify: identifyService()
+      }
+    })
+
     const helia = await createHelia({
       blockstore,
       datastore,
+      libp2p,
     })
 
     this.helia = helia

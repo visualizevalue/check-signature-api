3c89a2381dc88ab62ee955d819857557a5570345
jalil <jalil@1001.digital>
2023-11-25 04:40:44 +0100

    Pin files

--- app/Services/IPFS.ts
+++ app/Services/IPFS.ts
@@ -70,7 +70,6 @@ class IFPS {
             '/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa',
             '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
             '/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt',
-            '/ip4/104.131.131.82/tcp/4001/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ',
           ]
         })
       ],
@@ -93,7 +92,10 @@ class IFPS {
   async addJson (data: any): Promise<CID> {
     await this.setup()
 
-    return await this.json.add(data)
+    const cid = await this.json.add(data)
+    await this.helia.pins.add(cid)
+
+    return cid
   }
 
   async get (cid: CID): Promise<any> {
